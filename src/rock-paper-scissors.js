a3f6ec936cb53437a7e9eadf0d86d0e5d6f1e08d
Giacomo Venturini <venturinigiacomo@hotmail.it>
2018-05-17 23:13:41 +0200

    es 1/5 5 da rivedere

--- src/rock-paper-scissors.js
+++ src/rock-paper-scissors.js
@@ -3,24 +3,56 @@ class RockPaperScissors {
         /* ... costruisco i vari componenti che
            devono interagire */
         this.mainElement = document.createElement('div');
+        //add ui container classes
+        this.mainElement.classList.add("ui", "container", "grid", "centered");
         const title = document.createElement('h2');
+        title.classList.add("ui", "header");
         title.textContent = "Your Game";
         const button = document.createElement('button');
+        button.classList.add("ui", "button");
         button.textContent = "Play!";
-        this.mainElement.append(title);
+
+        const row = document.createElement('div');
+        const column = document.createElement('div');
+        column.classList.add("three", "wide", "column", "centered");
+        row.classList.add("row");
+        column.append(title);
+        row.append(column);
+        this.mainElement.append(row);
+        
+        const columni= document.createElement('div');
+        columni.classList.add("three", "wide","column", "centerd");
+
         this.labels = [ "Rock", "Paper", "Scissors" ];
         for (let i = 0; i < 3; i++) {
+
+            const rowi = document.createElement('div');
+            
+            
+            rowi.classList.add("row");
             const radioButton = document.createElement('input');
             radioButton.setAttribute('type', 'radio');
             radioButton.setAttribute('name', 'choice');
             radioButton.setAttribute('value', this.labels[i]);
-            const label = document.createElement('label');
-            label.textContent = this.labels[i];
-            this.mainElement.append(radioButton);
-            this.mainElement.append(label);
-            this.mainElement.append(document.createElement('br'));
+            const label = document.createElement('i');
+            label.classList.add("ui", "image", "label", "hand", this.labels[i].toLowerCase(), "icon", "huge");
+            label.textContent = " " + this.labels[i];
+
+            rowi.append(radioButton);
+            rowi.append(label);
+            columni.append(rowi);
+            
+            //this.mainElement.append(radioButton);
+            //this.mainElement.append(label);
+            //this.mainElement.append(document.createElement('br'));
         }
-        this.mainElement.append(button);
+        this.mainElement.append(columni);
+        //this.mainElement.append(button);
+        const rowf = document.createElement('div');
+        rowf.classList.add("row");
+        rowf.append(button);
+        columni.append(rowf);
+
         button.addEventListener('click', this.buttonPressed.bind(this));
         this.result = document.createElement('div');
         this.mainElement.append(this.result);
